31b13cda5d35ccf0bbe9b2270281d152e0b75802
Michael Adams <me@michdavidadams.com>
2024-01-02 17:46:23 -0500

    removed home.sh because desktop only needs it

--- sway/home.sh
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env bash
- 
-case "$(echo -e "󰛨 Lights on\n󰹐 Lights off" | bemenu $@ \
-    "Power:" -l 2)" in
-        "󰛨 Lights on") exec mosquitto_pub -t 'zigbee2mqtt/living_room/set' -m '{ "state": "ON" }';;
-        "󰹐 Lights off") exec mosquitto_pub -t 'zigbee2mqtt/living_room/set' -m '{ "state": "OFF" }';;
-esac
