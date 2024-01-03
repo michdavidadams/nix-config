2fe68be8b83bb7a44ace657c79996b52a26d3e5c
Michael Adams <me@michdavidadams.com>
2024-01-03 15:59:37 -0500

    sunset script for lights

--- desktop/sunset.sh
+++ desktop/sunset.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
-if [ $(sunwait poll 34.260460N -85.180618W) == "NIGHT" ]
+if [ $(sunwait poll 34.26N 85.18W) == "NIGHT" ]
 then
   exec mosquitto_pub -p 1883 -t 'zigbee2mqtt/living_room/set' -m '{ "scene_recall": 2 }'
 else
